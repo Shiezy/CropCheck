9dcbfba36ac37d2f152272091d023ba5b6bf4c9e
Kace-Chege <kigsche4@gmail.com>
2019-06-29 18:05:48 +0300

    Dashboard

--- app/src/main/java/com/example/cropcheck/MainActivity.java
+++ app/src/main/java/com/example/cropcheck/MainActivity.java
@@ -1,25 +1,42 @@
 package com.example.cropcheck;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView username, natid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button)findViewById(R.id.buttoncheck);
+//        Button btn = (Button)findViewById(R.id.buttoncheck);
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+//            }
+//        });
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            }
-        });
+        username = findViewById(R.id.username);
+        natid = findViewById(R.id.natid);
+
+        //import fonts
+        Typeface Mlight = Typeface.createFromAsset(getAssets(), "fonts/ML.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MM.ttf");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(), "fonts/MR.ttf");
+
+        //customize fonts
+        username.setTypeface(MMedium);
+        natid.setTypeface(Mlight);
+
     }
 }
