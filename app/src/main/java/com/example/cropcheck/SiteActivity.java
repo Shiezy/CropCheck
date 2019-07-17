e6dc214cff885f31c225bd6345d2b89759845fb8
Sheila <sheilagithinji@gmail.com>
2019-07-17 11:35:54 +0300

    site activity

--- app/src/main/java/com/example/cropcheck/SiteActivity.java
+++ app/src/main/java/com/example/cropcheck/SiteActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +20,7 @@ import com.example.cropcheck.services.SiteService;
 import com.example.cropcheck.services.UserService;
 import com.example.cropcheck.utils.CoreUtils;
 
+import java.util.Date;
 import java.util.List;
 
 import retrofit2.Call;
@@ -30,11 +33,14 @@ public class SiteActivity extends AppCompatActivity {
     Integer user_id;
     PolicyAdapter adapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         RecyclerView recyclerView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_site);
+        final TextView season_dets = (TextView) findViewById(R.id.season_dets);
+        final TextView season_start_date = (TextView) findViewById(R.id.season_start_date);
 
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
@@ -43,7 +49,7 @@ public class SiteActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "SITE ID is null" , Toast.LENGTH_SHORT).show();
             } else {
                 site_id = extras.getInt("id");
-                Toast.makeText(getApplicationContext(), ""+site_id , Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), ""+site_id , Toast.LENGTH_SHORT).show();
             }
         } else {
             site_id = (Integer) savedInstanceState.getSerializable("id");
@@ -66,7 +72,9 @@ public class SiteActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
+                            loadSeasonDets();
                             loadPolicies();
+
                         }
                     });
 
@@ -79,22 +87,7 @@ public class SiteActivity extends AppCompatActivity {
             }
         });
 
-        Call<Season> season = CoreUtils.getAuthRetrofitClient(getToken()).create(SeasonService.class).isOnSeason(site_id);
-        season.enqueue(new Callback<Season>() {
-            @Override
-            public void onResponse(Call<Season> call, Response<Season> response) {
-                if(response.isSuccessful()){
-                    Boolean status = response.body().getStatus();
-                    Toast.makeText(getApplicationContext(),status.toString(),Toast.LENGTH_SHORT).show();
 
-                }else   Toast.makeText(getApplicationContext(),response.errorBody().toString(),Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onFailure(Call<Season> call, Throwable t) {
-
-            }
-        });
     }
     public String getToken(){
         return PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("ACCESS_TOKEN", null);
@@ -120,6 +113,68 @@ public class SiteActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<List<Policy>> call, Throwable t) {
 
+            }
+        });
+    }
+    private void loadSeasonDets(){
+        final TextView season_dets = (TextView) findViewById(R.id.season_dets);
+        final TextView season_start_date = (TextView) findViewById(R.id.season_start_date);
+        Toast.makeText(getApplicationContext(), ""+site_id , Toast.LENGTH_SHORT).show();
+
+
+        Call<Season> season = CoreUtils.getAuthRetrofitClient(getToken()).create(SeasonService.class).isOnSeason(site_id);
+        season.enqueue(new Callback<Season>() {
+            @Override
+            public void onResponse(Call<Season> call, final Response<Season> response) {
+                if(response.isSuccessful()){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            int status = response.body().getStatus();
+                            String startDate = response.body().getStart_date();
+
+                            if(status == 1){
+                                season_dets.setText("Has been in season since");
+                                season_start_date.setText(startDate);
+                            } else {
+                                season_dets.setText("Not in Season");
+                                season_start_date.setVisibility(View.GONE);
+                            }
+
+
+
+                        }
+                    });
+
+
+
+                }
+                else  {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            season_dets.setText("monica");
+
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Season> call, final Throwable t) {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Log.d("TEMP_TAG", t.getMessage());
+
+                        season_dets.setText("magda");
+
+                    }
+                });
             }
         });
     }
